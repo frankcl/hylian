081c8ce79556460a6685ae2abe991eee4365e639
frankcl <frank.lichen@gmail.com>
2023-08-29 17:55:26 +0800

    add UserService interface

---
+++ sso-server/src/main/java/xin/manong/house/keeper/sso/service/request/UserSearchRequest.java
@@ -0,0 +1,40 @@
+package xin.manong.house.keeper.sso.service.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * 用户搜索请求
+ *
+ * @author frankcl
+ * @date 2023-03-21 16:41:18
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserSearchRequest extends SearchRequest {
+
+    /**
+     * 用户ID
+     */
+    @JsonProperty("user_id")
+    public String userId;
+    /**
+     * 用户名
+     */
+    @JsonProperty("user_name")
+    public String userName;
+    /**
+     * 真实名称
+     */
+    @JsonProperty("name")
+    public String name;
+    /**
+     * 租户ID
+     */
+    @JsonProperty("tenant_id")
+    public String tenantId;
+    /**
+     * 供应商ID
+     */
+    @JsonProperty("vendor_id")
+    public String vendorId;
+}
