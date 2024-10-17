b607365c22e2395c692fe81165b200c829b61e27
frankcl <frank.lichen@gmail.com>
2024-10-17 22:38:37 +0800

    fix bugs and optimize codes

--- hylian-server/db/hylian.sql
+++ hylian-server/db/hylian.sql
@@ -11,17 +11,17 @@
  Target Server Version : 80030 (8.0.30)
  File Encoding         : 65001
 
- Date: 26/09/2024 14:12:23
+ Date: 17/10/2024 22:38:11
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for active_record
+-- Table structure for activity
 -- ----------------------------
-DROP TABLE IF EXISTS `active_record`;
-CREATE TABLE `active_record` (
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE `activity` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `session_id` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
@@ -37,18 +37,19 @@ CREATE TABLE `active_record` (
   KEY `INDEX_ID` (`id`) USING BTREE,
   KEY `INDEX_SESSION_ID` (`session_id`) USING BTREE,
   KEY `INDEX_TICKET_ID` (`ticket_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=147 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=385 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for app
 -- ----------------------------
 DROP TABLE IF EXISTS `app`;
 CREATE TABLE `app` (
-  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `secret` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `create_time` bigint NOT NULL,
-  `update_time` bigint NOT NULL,
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '应用ID',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '应用名',
+  `secret` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '应用秘钥',
+  `create_time` bigint NOT NULL COMMENT '创建时间',
+  `update_time` bigint NOT NULL COMMENT '更新时间',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '应用描述',
   PRIMARY KEY (`id`),
   KEY `INDEX_APP_ID` (`id`) USING BTREE,
   KEY `INDEX_APP_NAME` (`name`) USING BTREE,
@@ -57,22 +58,39 @@ CREATE TABLE `app` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
+-- Table structure for app_user
+-- ----------------------------
+DROP TABLE IF EXISTS `app_user`;
+CREATE TABLE `app_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `app_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `create_time` bigint NOT NULL,
+  `update_time` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `INDEX_APP_ID` (`app_id`) USING BTREE,
+  KEY `INDEX_USER_ID` (`user_id`) USING BTREE,
+  KEY `INDEX_CREATE_TIME` (`create_time`) USING BTREE,
+  KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1846918742596968450 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- ----------------------------
 -- Table structure for permission
 -- ----------------------------
 DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission` (
   `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `resource` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `app_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `create_time` bigint NOT NULL,
   `update_time` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `INDEX_NAME` (`name`) USING BTREE,
-  KEY `INDEX_RESOURCE` (`resource`) USING BTREE,
   KEY `INDEX_APP_ID` (`app_id`) USING BTREE,
   KEY `INDEX_CREATE_TIME` (`create_time`) USING BTREE,
-  KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE
+  KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE,
+  KEY `INDEX_PATH` (`path`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
@@ -97,7 +115,7 @@ CREATE TABLE `role` (
 -- ----------------------------
 DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE `role_permission` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `role_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `permission_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `create_time` bigint NOT NULL,
@@ -107,7 +125,7 @@ CREATE TABLE `role_permission` (
   KEY `INDEX_PERMISSION_ID` (`permission_id`) USING BTREE,
   KEY `INDEX_CREATE_TIME` (`create_time`) USING BTREE,
   KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1846519855931551746 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for tenant
@@ -116,13 +134,11 @@ DROP TABLE IF EXISTS `tenant`;
 CREATE TABLE `tenant` (
   `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `vendor_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `create_time` bigint NOT NULL,
   `update_time` bigint NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   KEY `INDEX_CREATE_TIME` (`create_time`) USING BTREE,
   KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE,
-  KEY `INDEX_VENDOR_ID` (`vendor_id`) USING BTREE,
   KEY `INDEX_TENANT_ID` (`id`) USING BTREE,
   KEY `INDEX_TENANT_NAME` (`name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -133,22 +149,22 @@ CREATE TABLE `tenant` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `create_time` bigint NOT NULL,
   `update_time` bigint NOT NULL,
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `tenant_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `vendor_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `disabled` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `INDEX_USER_NAME` (`user_name`) USING BTREE,
+  UNIQUE KEY `INDEX_USER_NAME` (`username`) USING BTREE,
   KEY `INDEX_NAME` (`name`) USING BTREE,
   KEY `INDEX_CREATE_TIME` (`create_time`) USING BTREE,
   KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE,
   KEY `INDEX_TENANT_ID` (`tenant_id`) USING BTREE,
-  KEY `INDEX_VENDOR_ID` (`vendor_id`) USING BTREE,
-  KEY `INDEX_USER_ID` (`id`) USING BTREE
+  KEY `INDEX_USER_ID` (`id`) USING BTREE,
+  KEY `INDEX_DISABLE` (`disabled`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
@@ -168,22 +184,6 @@ CREATE TABLE `user_role` (
   KEY `INDEX_APP_ID` (`app_id`) USING BTREE,
   KEY `INDEX_CREATE_TIME` (`create_time`) USING BTREE,
   KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1835699025827876867 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- ----------------------------
--- Table structure for vendor
--- ----------------------------
-DROP TABLE IF EXISTS `vendor`;
-CREATE TABLE `vendor` (
-  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `create_time` bigint NOT NULL,
-  `update_time` bigint NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `INDEX_VENDOR_NAME` (`name`) USING BTREE,
-  KEY `INDEX_CREATE_TIME` (`create_time`) USING BTREE,
-  KEY `INDEX_UPDATE_TIME` (`update_time`) USING BTREE,
-  KEY `INDEX_VENDOR_ID` (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1846194229286604803 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
