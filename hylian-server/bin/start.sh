8bac5391e85ec255d18ce2c1ef25201c8dc6db20
frankcl <frank.lichen@gmail.com>
2025-05-27 09:39:52 +0800

    add package scripts

--- hylian-server/bin/start.sh
+++ hylian-server/bin/start.sh
@@ -6,4 +6,5 @@ mkdir log
 
 exec java -Xlog:gc=info:file=./log/gc.log:uptimemillis,pid:filecount=5,filesize=1m -XX:+UseG1GC \
     -Xms100m -Xmx300m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./log/heap.dump \
-    -cp "../jars/*" -Dlog4j.configuration="file:../conf/log4j.properties" xin.manong.hylian.server.Application > stderr.out 2>&1 & disown
+    -cp "../lib/*" -Dlog4j.configuration="file:../conf/log4j.properties" \
+    xin.manong.hylian.server.Application > stderr.out 2>&1 & disown
