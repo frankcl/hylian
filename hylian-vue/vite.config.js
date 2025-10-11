509c2b8c855ffcf09178b84ab56edf83908b3132
frankcl <frank.lichen@gmail.com>
2025-10-11 10:14:11 +0800

    modify domain to lumychip.com

--- hylian-vue/vite.config.js
+++ hylian-vue/vite.config.js
@@ -46,7 +46,7 @@ export default defineConfig({
     cors: true,
     proxy: {
       '^/api': {
-        target: 'https://hylian.junctionmagic.com:9001',
+        target: 'https://hylian.lumychip.com:9001',
         changeOrigin: true,
         ws: true,
         https: true,
